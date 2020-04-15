beaed32344eef7373cf7eeb1558ce05e9336bf75
Rahmet <ruslanovrahmet@gmail.com>
2020-04-15 23:57:59 +0500

    Hide div of Special Offer using if statement

--- main/travello/views.py
+++ main/travello/views.py
@@ -8,18 +8,21 @@ def index(request):
     dest1.desc = 'The City That Never Sleeps'
     dest1.price = 700
     dest1.img = 'destination_1.jpg'
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Indonesia'
     dest2.desc = 'The best Country on the World'
     dest2.price = 650
     dest2.img = 'destination_2.jpg'
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'San Francisco'
     dest3.desc = 'The more beautifull City'
     dest3.price = 750
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
