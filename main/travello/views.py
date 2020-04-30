b85b3f0434e0a0be7e4edfdc55bc2017668e0841
Rahmet <ruslanovrahmet@gmail.com>
2020-05-01 03:11:32 +0500

    Connecting to database

--- main/travello/views.py
+++ main/travello/views.py
@@ -3,27 +3,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Bali'
-    dest1.desc = 'The City That Never Sleeps'
-    dest1.price = 700
-    dest1.img = 'destination_1.jpg'
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Indonesia'
-    dest2.desc = 'The best Country on the World'
-    dest2.price = 650
-    dest2.img = 'destination_2.jpg'
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = 'San Francisco'
-    dest3.desc = 'The more beautifull City'
-    dest3.price = 750
-    dest3.img = 'destination_3.jpg'
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, "index.html", {'dests': dests})
