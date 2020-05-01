28bfd59bea0c92c4e59df99748fa28fd89d2aea1
Rahmet <ruslanovrahmet@gmail.com>
2020-05-02 02:04:52 +0500

    Done login and logout

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials!!!')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -21,9 +37,14 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save();
+                return redirect('login')
         else:
             messages.info(request, 'password not matching...')
             return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
